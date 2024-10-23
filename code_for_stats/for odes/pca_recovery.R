5c4710a21ee14a6106fe71eb3b9a2703396a8516
unknown <erwilson@mun.ca>
2024-10-23 11:28:39 -0230

    pca for parameters

--- code_for_stats/for odes/pca_recovery.R
+++ code_for_stats/for odes/pca_recovery.R
@@ -11,21 +11,25 @@ g_vs_fparm2 <- list()
 for(i in 1:length(parms)) {
   nam <- names(parms)[i]
   g_vs_fparm2[[i]] <- assign(nam,unlist(map(g_vs_fparm$parameters, pluck, i)))
-  names(g_vs_fparm2) <- names(parms)
+
 }
+
+names(g_vs_fparm2) <- names(parms)
 
 g_vs_fparm2df <- as.data.frame(g_vs_fparm2) %>% 
   cbind(g_vs_fparm)
 
-n_g_vs_f <- g_vs_f %>% 
-  group_by(as.factor(Moose), as.factor(By), as.factor(g_or_f), .drop=FALSE) %>% 
+saveRDS(g_vs_fparm2df, file = "~/C_forecasting_NL/code_for_stats/for odes/outputs/g_vs_fparm2df.RDS")
+
+n_g_vs_fparm <- g_vs_fparm2df %>% 
+  group_by_at(vars(names(parms), g_or_f), .drop=FALSE) %>% 
   count(.drop=FALSE) %>% 
-  ungroup() %>% 
-  mutate(Moose = as.numeric(as.character(`as.factor(Moose)`))) %>% 
-  mutate(By = as.numeric(as.character(`as.factor(By)`))) %>% 
-  rename(g_or_f = `as.factor(g_or_f)`) 
+  ungroup() 
+
+# into %
+n_fsparm <- n_g_vs_fparm %>% 
+  subset( g_or_f == " forest") %>% 
+  mutate(percent_recover = (n/nrow(g_vs_fparm2df)*100) )
 
 
-pca_res <- prcomp(df, scale. = TRUE)
 
-autoplot(pca_res)
